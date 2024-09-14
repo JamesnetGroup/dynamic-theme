a4a1818d0aad598a5835dc61d0fa89e5210cbc0f
vickyqu115 <vickyqu115@gmail.com>
2024-09-15 07:52:01 +0800

    Add src

---
+++ src/DynamicTheme.Core/ThemeManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace DynamicTheme.Core
+{
+    public class ThemeManager
+    {
+        private Dictionary<string, ResourceDictionary> _themes = new();
+
+        public void RegisterTheme(string themeName, string assemblyName, string resourcePath)
+        {
+            string uri = $"/{assemblyName};component/{resourcePath}";
+
+            ResourceDictionary resource = new ResourceDictionary();
+            resource.Source = new Uri(uri, UriKind.RelativeOrAbsolute);
+
+            _themes.Add(themeName, resource);
+        }
+
+        public void ApplyTheme(string themeName)
+        {
+            ResourceDictionary resource = _themes[themeName];
+
+            Application.Current.Resources.MergedDictionaries.Add(resource);
+        }
+    }
+}
